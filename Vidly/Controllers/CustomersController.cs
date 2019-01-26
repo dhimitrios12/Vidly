d203c2626cc975c1422a0bddbb20fff983f9fe16
Dhimitrios Sula <dhimitriossula@hotmail.com>
2019-01-26 21:54:33 +0100

    Exercize 1 finished

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+	public class CustomersController : Controller
+	{
+		// GET: Customers
+		public ActionResult Index()
+		{
+			return View(GetCustomers());
+		}
+
+		[Route("customers/details/{id}")]
+		public ActionResult Details(int id)
+		{
+			var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+
+			return View(customer);
+		}
+
+		public List<Customer> GetCustomers()
+		{
+			List<Customer> customers = new List<Customer>
+			{
+				new Customer {Id = 1, Name = "Mark Benkol"},
+				new Customer {Id = 2, Name = "Lompard Tendina"}
+			};
+
+			return customers;
+		}
+	}
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+	public class MoviesController : Controller
+	{
+		// GET: Movies
+		public ActionResult Index()
+		{
+			return View(GetMovies());
+		}
+
+		public List<Movie> GetMovies()
+		{
+			List<Movie> movies = new List<Movie>
+			{
+				new Movie {Id = 1, Name = "Grinch"},
+				new Movie {Id = 2, Name = "Ralph breakes the internet"}
+			};
+
+			return movies;
+		}
+	}
+}
