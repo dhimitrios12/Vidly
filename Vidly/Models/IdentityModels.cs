3fab9fbc772ce2e949c5183b137fa0007731d821
Dhimitrios Sula <dhimitriossula@hotmail.com>
2019-01-27 18:25:17 +0100

    Added identity migrations

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
